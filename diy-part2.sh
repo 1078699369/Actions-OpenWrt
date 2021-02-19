b59fa3f5f3a0e5e14b28669ee1debb5de42b4e38
1078699369 <1078699369@qq.com>
2021-02-19 19:14:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,6 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.251/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.252/g' package/base-files/files/bin/config_generate
 #京东签到
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git  package/mine/luci-app-jd-dailybonus
+# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git  package/mine/luci-app-jd-dailybonus
